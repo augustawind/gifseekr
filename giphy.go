47b78404e517220d82f5a3fb7a6ed2df92d4ba4d
Dustin Rohde <dustin.rohde@gmail.com>
2019-09-07 04:04:14 -0400

    create basic giphy framework

--- giphy.go
+++ giphy.go
@@ -1,12 +1,136 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/davecgh/go-spew/spew"
 	"github.com/go-resty/resty/v2"
 )
 
 const apiKey = "jEYeMjhDmvnJpAsKEIauvWzWyPae6wwm"
+const giphySearchURL = "https://api.giphy.com/v1/gifs/search"
+
+type Rating string
+
+const (
+	RatingY    Rating = "Y"
+	RatingG    Rating = "G"
+	RatingPG   Rating = "PG"
+	RatingPG13 Rating = "PG-13"
+	RatingR    Rating = "R"
+)
+
+type Lang string
+
+const (
+	LangEN Lang = "en"
+)
+
+type GiphyClient struct {
+	client   *resty.Client
+	apiKey   string
+	pageSize int
+	lang     Lang
+}
+
+func NewGiphyClient(apiKey string) *GiphyClient {
+	httpClient := resty.New()
+	return &GiphyClient{
+		client:   httpClient,
+		apiKey:   apiKey,
+		pageSize: 25,
+		lang:     LangEN,
+	}
+}
+
+func (c *GiphyClient) PageSize(pageSize int) *GiphyClient {
+	c.pageSize = pageSize
+	return c
+}
+
+func (c *GiphyClient) Lang(lang Lang) *GiphyClient {
+	c.lang = lang
+	return c
+}
+
+func (c *GiphyClient) search(query string, page int) (*resty.Response, error) {
+	req := c.client.R().
+		SetQueryParams(map[string]string{
+			"q":       query,
+			"api_key": c.apiKey,
+			"lang":    string(c.lang),
+			"limit":   strconv.Itoa(c.pageSize),
+			"offset":  strconv.Itoa(page * c.pageSize),
+		})
+	spew.Dump(req.QueryParam)
+	return req.Get(giphySearchURL)
+}
+
+func (c *GiphyClient) Search(query string) *SearchHandle {
+	return &SearchHandle{
+		client: c,
+		query:  query,
+		page:   0,
+	}
+}
+
+type SearchHandle struct {
+	client *GiphyClient
+	query  string
+	page   int
+}
+
+func (h *SearchHandle) Next() (*SearchResult, error) {
+	// Perform search
+	resp, err := h.client.search(h.query, h.page)
+	if err != nil {
+		return nil, err
+	}
+
+	// Assert request succeeded
+	if resp.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("search failed: status=%s, body=%s", resp.Status(), resp.Body())
+	}
+
+	// Parse search results
+	var searchResult SearchResult
+	err = json.Unmarshal(resp.Body(), &searchResult)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing search results: %s", resp.Body())
+	}
+
+	h.page += 1
+	return &searchResult, nil
+}
+
+type SearchResult struct {
+	Data       []GIFObject
+	Pagination PaginationObject
+}
+
+type GIFObject struct {
+	Title    string
+	Url      string
+	BitlyUrl string `json:"bitly_url"`
+}
+
+type PaginationObject struct {
+	Offset     int
+	TotalCount int `json:"total_count"`
+	Count      int
+}
 
 func main() {
-	client := resty.New()
-	_ = client
+	client := NewGiphyClient(apiKey).PageSize(2)
+	handle := client.Search("food")
+	resp, err := handle.Next()
+	if err != nil {
+		spew.Dump("ERROR: ", err)
+		spew.Dump("BODY: ", resp)
+	} else {
+		spew.Dump(resp)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/dustinrohde/gifseekr
 
 go 1.12
 
-require github.com/go-resty/resty/v2 v2.0.0
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-resty/resty/v2 v2.0.0
+	github.com/hpcloud/tail v1.0.0 // indirect
+	github.com/jarcoal/httpmock v1.0.4 // indirect
+	github.com/onsi/ginkgo v1.10.1 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
